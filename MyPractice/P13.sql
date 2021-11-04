d164a111b1e769f1b75fc844c16bd51c4f48c060
ChenJH <18601277500@163.com>
2021-11-04 18:44:20 +0800

    完成第31题

--- MyPractice/P13.sql
+++ MyPractice/P13.sql
@@ -1,1 +1,14 @@
 -- 13. 按平均成绩从高到低显示所有学生的所有课程的成绩以及平均成绩
+
+SELECT SC.SId,SC.score,AvgSC.scoreAvg
+FROM(
+    SELECT SId,AVG(score) as scoreAvg
+    FROM SC
+    GROUP BY SId
+) AvgSC
+JOIN SC
+ON SC.SId=AvgSC.SId
+-- 先根据平均分降序，平均分一样的根据分数来降序
+ORDER BY  AvgSC.scoreAvg DESC,SC.score DESC
+
+;
